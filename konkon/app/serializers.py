38795a01ceb6f7dc6fb7c385363e4296bc6bcb65
hiki7 <rsaliyev03@gmail.com>
2024-10-06 20:19:11 +0500

    add: genre serializer

--- konkon/app/serializers.py
+++ konkon/app/serializers.py
@@ -1,8 +1,14 @@
 from rest_framework import serializers
-from .models import Anime, Category, Genre, UserAnime
+from .models import Anime, Category, Genre
 
 
 class CategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = Category
         fields = ["title", "description"]
+
+
+class GenreSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Genre
+        fields = ["name"]
