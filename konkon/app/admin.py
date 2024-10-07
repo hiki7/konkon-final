00f98fd35bb05af31ac55c71a7b522b0c577df34
hiki7 <rsaliyev03@gmail.com>
2024-10-07 22:05:00 +0500

    add: anime admin

--- konkon/app/admin.py
+++ konkon/app/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import Anime, Category, Genre, UserAnime
 
-# Register your models here.
+
+@admin.register(Anime)
+class AnimeAdmin(admin.ModelAdmin):
+    list_display = (
+        "title",
+        "synopsis",
+        "status",
+        "start_date",
+        "end_date",
+        "episode_count",
+        "show_type",
+        "age_rating",
+    )
+    search_fields = ("title", "status", "show_type", "age_rating")
+    list_filter = ("status", "episode_count", "show_type", "age_rating")
+    ordering = ("title", "episode_count")
